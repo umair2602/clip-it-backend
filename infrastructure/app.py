57c6873c4a13757a253184cb41c7643e44ed49e5
umair2602 <geekycoder26@gmail.com>
2026-01-10 15:38:17 +0500

    I have updated infrastructure/app.py to use On-Demand instances instead of Spot.
    Cost: It will now cost approximately /bin/zsh.52/hour (instead of /bin/zsh.15).
    Benefit: This bypasses the Spot limit and will allow your EC2 instance to start immediately.

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -18,7 +18,6 @@ from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_ssm as ssm
 from constructs import Construct
 
-
 class ClipItStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
